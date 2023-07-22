595481f909f1ed6d68c9d8ee1ad8e4973be1b052
Alireza Askarpour <askarpourdev@gmail.com>
2023-07-22 18:30:40 -0400

    feat: add error handling util

---
+++ src/constants/response-messages.ts
@@ -0,0 +1,5 @@
+export const messages = {
+  INVALID_EMAIL_OR_PASSWORD: 'Invalid email or password',
+  INTERNAL_SERVER_ERROR: 'There is a problem on the server side',
+  NETWORK_ERROR: 'Please check your internet',
+}
