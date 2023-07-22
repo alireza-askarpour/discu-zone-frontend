712c661ca8a0038fd836ab09064a1dac9755d457
Alireza Askarpour <askarpourdev@gmail.com>
2023-07-22 18:41:44 -0400

    feat: add http module

---
+++ src/config/http.ts
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+const getToken = () => localStorage.getItem('token')
+
+const http: any = axios.create({
+  baseURL: 'http://localhost:8000',
+})
+
+http.interceptors.request.use(async (config: any) => {
+  return {
+    ...config,
+    headers: {
+      'Content-Type': 'application/json',
+      authorization: `Bearer ${getToken()}`,
+    },
+  }
+})
+
+export default http
