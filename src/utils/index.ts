f1ebdc5eeb04768a27674335e7eae1b3d446f5bb
Alireza Askarpour <askarpourdev@gmail.com>
2023-07-22 18:34:21 -0400

    faet: add has network util

---
+++ src/utils/index.ts
@@ -0,0 +1,4 @@
+export { classNames } from './classNames'
+export { errorHandling } from './error-handling'
+export { getFirstCharacter } from './get-first-character'
+export { hasNetwork } from './has-network'
